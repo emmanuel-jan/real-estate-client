0fbb6a2127c43aa757b3469777835d4a56d50663
brian <briankathukya2000@gmail.com>
2021-05-05 17:01:35 +0300

    Merged conflicts

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  // declare a class that will carry form data
+  registerData;
+
+  constructor(
+    private formbuilder: FormBuilder,
+    private auth: AuthService,
+    private router: Router
+
+  ) {
+    this.registerData = this.formbuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      phoneNumber: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+
+    })
+  }
+
+  register() {
+    //call the service to save the token
+
+    this.auth.saveUser(this.registerData.value).subscribe(
+      (res) => {
+        console.log(res);
+        this.router.navigate(['/login']);
+      },
+      (err) => {
+        console.log(err);
+      }
+    )
+  }
 
   ngOnInit(): void {
+
   }
 
 }
